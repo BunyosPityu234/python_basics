c281dde7d53951bff3b9822a46ae30d7ec130b5d
Szilágyi Gábor <gatymen2013@gmail.com>
2024-10-02 13:51:17 +0200

    Learn about for and while loops

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
-a: bool = True
-b: bool = False
+""" a: bool = True
+b: bool = False """
 
 # Learn if statement
 """ if a == True:
@@ -9,3 +9,18 @@ elif b == True:
 else:
     print("neither a or b is True")
  """
+
+# What is for loop
+""" for i in range(5):
+    pass
+ """
+""" 
+message: str = "Give me your answer: "
+
+while True:
+    answer: str = input(message)
+    if len(answer) >= 3:
+        print("Thank you!")
+        break
+    else:
+        message = "Please give me at least 3 characters: " """
